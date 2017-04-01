1997a63a33ea5d4cd00773b02fc44e0705babb9a
gavinshao111 <yiyecheer@163.com>
2017-04-01 17:40:21 +0800

    为了支持RAII，通过构造函数构造对象，删除原来的 allocate、wrap方法，增加read only功能，等

--- ByteBuffer/src/ByteBuffer.cpp
+++ ByteBuffer/src/ByteBuffer.cpp
@@ -13,68 +13,116 @@
 
 #include "ByteBuffer.h"
 #include <string.h>
-#include <stdio.h>
+#include <iostream>
 #include <iomanip>
 
 using namespace bytebuf;
 using namespace std;
 
-//ByteBuffer::ByteBuffer(const size_t& size) : _hb(new byte[size]), _position(0), _limit(size), _capacity(size) {
-//}
-//
-//ByteBuffer::ByteBuffer(byte* src, const size_t& length) : _hb(src), _position(0), _limit(length), _capacity(length) {
-//}
 
-ByteBuffer::ByteBuffer(const size_t& pos, const size_t& lim, const size_t& cap, uint8_t* hb) :
-_hb(hb), _position(pos), _limit(lim), _capacity(cap) {
-    if (cap < 0)
+// public
+
+ByteBuffer::ByteBuffer(const size_t& capacity) {
+    new (this)ByteBuffer(0, capacity, capacity, new uint8_t[capacity], false);
+}
+// public
+
+ByteBuffer::ByteBuffer(uint8_t* src, const size_t& offset, const size_t& length) {
+    new (this)ByteBuffer(0, length, length, src + offset, true);
+}
+// public
+
+ByteBuffer::ByteBuffer(uint8_t* src, const size_t& length) {
+    new (this)ByteBuffer(src, 0, length);
+}
+// private
+
+ByteBuffer::ByteBuffer(const size_t& pos, const size_t& lim, const size_t& cap, uint8_t* hb, const bool& isWrapped) :
+m_hb(hb), m_position(pos), m_limit(lim), m_capacity(cap), m_isWarpped(isWrapped) {
+    if (cap == 0 || hb == NULL)
         throw ByteBufferException("ByteBuffer::ByteBuffer(): IllegalArgument");
 }
 
-ByteBuffer::ByteBuffer(const ByteBuffer& orig) : _hb(orig._hb), _position(orig._position), _limit(orig._limit), _capacity(orig._capacity) {
+ByteBuffer::ByteBuffer(const ByteBuffer& orig) : m_position(orig.m_position), m_limit(orig.m_limit), m_capacity(orig.m_capacity), m_isWarpped(orig.m_isWarpped) {
+    if (m_isWarpped)
+        m_hb = orig.m_hb;
+    else {
+        m_hb = new uint8_t[m_capacity];
+        memcpy(m_hb, orig.m_hb, m_capacity);
+    }
+    // test
+    cout << "copy construct" << endl;
 }
 
 ByteBuffer::~ByteBuffer() {
+    if (!m_isWarpped)
+        delete m_hb;
 }
+//ByteBuffer* ByteBuffer::allocate(const size_t& capacity) {
+//    return new ByteBuffer(0, capacity, capacity, new uint8_t[capacity]);
+//}
+//
+//void ByteBuffer::freeMemery(void) {
+//    if (_hb) {
+//        delete _hb;
+//        _hb = NULL;
+//    }
+//}
+//
+//ByteBuffer* ByteBuffer::wrap(uint8_t* array, const size_t& offset, const size_t& length, const size_t& capacity) {
+//    //checkBounds(offset, length, sizeof(array));
+//    try {
+//        return new ByteBuffer(offset, offset + length, capacity, array);
+//    } catch (exception &e) {
+//        throw ByteBufferException("ByteBuffer::wrap(): IndexOutOfBounds");
+//    }
+//}
+//
+//ByteBuffer* ByteBuffer::wrap(uint8_t* array, const size_t& capacity) {
+//    return wrap(array, 0, capacity, capacity);
+//}
 
 void ByteBuffer::flip(void) {
-    _limit = _position;
-    _position = 0;
+    m_limit = m_position;
+    m_position = 0;
 }
 
 void ByteBuffer::clear(void) {
-    _position = 0;
-    _limit = _capacity;
+    m_position = 0;
+    m_limit = m_capacity;
 }
 
 bool ByteBuffer::hasRemaining() const {
-    return _position < _limit;
+    return m_position < m_limit;
 }
 
-void ByteBuffer::checkBounds(const int& off, const int& len, const int& size) {
-    if ((off | len | (off + len) | (size - (off + len))) < 0)
-        throw ByteBufferException("ByteBuffer::checkBounds(): IndexOutOfBounds");
+//void ByteBuffer::checkBounds(const size_t& off, const size_t& len, const size_t& cap) {
+//    if ((off | len | (off + len) | (cap - (off + len))) < 0)
+//        throw ByteBufferException("ByteBuffer::checkBounds(): IndexOutOfBounds");
+//}
+
+void ByteBuffer::checkRemaining(const size_t& bufferOffset, const size_t& length) const {
+    if (m_position + bufferOffset + length > m_limit)
+        throw ByteBufferException("ByteBuffer::checkRemaining(): BufferOverflow");
 }
 
 size_t ByteBuffer::remaining(void) const {
-    return _limit - _position;
+    return m_limit - m_position;
 }
 
-void ByteBuffer::put(const uint8_t* src, const int& offset, const int& length) {
-    checkBounds(offset, length, offset + length);
-
-    if (length > remaining())
-        throw ByteBufferException("ByteBuffer::put(): BufferOverflow");
-
+void ByteBuffer::put(const uint8_t* src, const size_t& offset, const size_t& length) {
+    checkRemaining(0, length);
     size_t end = offset + length;
     for (size_t i = offset; i < end; i++)
         put(src[i]);
 }
 
 void ByteBuffer::put(const uint8_t& src) {
-    if (_position >= _limit)
-        throw ByteBufferException("ByteBuffer::put(): BufferOverflow");
-    _hb[_position++] = src;
+    if (m_readOnly)
+        throw ByteBufferException("ByteBuffer::put(): read only");
+    
+    checkRemaining(0, 1);
+    m_hb[m_position++] = src;
 }
 
 void ByteBuffer::put(const uint16_t& src) {
@@ -93,64 +141,58 @@ void ByteBuffer::put(const char* src) {
     put((uint8_t*) src, 0, strlen(src));
 }
 
-const uint8_t& ByteBuffer::get(const size_t& index) const {
-    checkBounds(index, 1, _capacity);
-    return _hb[index];
+void ByteBuffer::put(const ByteBuffer& src, const size_t& offset, const size_t& length) {
+    put(src.array(), src.position() + offset, src.remaining());
 }
+
+void ByteBuffer::put(const ByteBuffer& src) {
+    put(src.array(), src.position(), src.remaining());
+}
+
+const uint8_t& ByteBuffer::get(const size_t& index) const {
+    if (index > m_capacity)
+        throw ByteBufferException("ByteBuffer::get(): IllegalArgument");
+    
+    return m_hb[index];
+}
+
 
 const uint8_t& ByteBuffer::get() {
-    return get(_position++);
+    return get(m_position++);
 }
 
-ByteBuffer* ByteBuffer::allocate(const size_t& capacity) {
-    if (capacity < 0)
-        throw ByteBufferException("ByteBuffer::allocate(): IllegalArgument");
-    return new ByteBuffer(0, capacity, capacity, new uint8_t[capacity]);
-}
-
-void ByteBuffer::freeMemery(void) {
-    if (_hb) {
-        delete _hb;
-        _hb = NULL;
-    }
-}
-
-ByteBuffer* ByteBuffer::wrap(uint8_t* array, const size_t& offset, const size_t& length, const size_t& capacity) {
-    //checkBounds(offset, length, sizeof(array));
-    try {
-        new ByteBuffer(offset, offset + length, capacity, array);
-    } catch (exception &e) {
-        throw ByteBufferException("ByteBuffer::wrap(): IndexOutOfBounds");
-    }
-}
-
-ByteBuffer* ByteBuffer::wrap(uint8_t* array, const size_t& capacity) {
-    return wrap(array, 0, capacity, capacity);
+const uint16_t& ByteBuffer::getShort() {
+    checkRemaining(0, 2);
+    return *(uint16_t*)m_hb;    
 }
 
 const size_t& ByteBuffer::position(void) const {
-    return _position;
+    return m_position;
 }
 
-size_t ByteBuffer::position(const size_t& newPosition) {
-    if ((newPosition > _limit) || (newPosition < 0))
+void ByteBuffer::position(const size_t& newPosition) {
+    if ((newPosition > m_limit))
         throw ByteBufferException("ByteBuffer::movePosition(): IllegalArgument");
-    _position = newPosition;
+    
+    m_position = newPosition;
 }
 
 void ByteBuffer::movePosition(const size_t& length, const bool isReverse/* = false*/) {
-    size_t newPos = isReverse ? (_position - length) : (_position + length);
-    if ((newPos > _limit) || (newPos < 0))
+    size_t newPos = isReverse ? (m_position - length) : (m_position + length);
+    if ((newPos > m_limit))
         throw ByteBufferException("ByteBuffer::movePosition(): IllegalArgument");
-    _position = newPos;
+    
+    m_position = newPos;
 }
 
-void ByteBuffer::modifyMemery(const uint8_t* src, const size_t& offset, const size_t& length, const size_t& position) {
-    if (_position + length > _limit)
-        throw ByteBufferException("ByteBuffer::modifyMemery(): IllegalArgument");
-    for (size_t i = 0; i < length; i++)
-        _hb[i + position] = src[offset + i];
-}
+//void ByteBuffer::modifyMemery(const uint8_t* src, const size_t& offset, const size_t& length, const size_t& position) {
+//    if (m_readOnly)
+//        throw ByteBufferException("ByteBuffer::put(): read only");
+//    if (m_position + length > m_limit)
+//        throw ByteBufferException("ByteBuffer::modifyMemery(): IllegalArgument");
+//    for (size_t i = 0; i < length; i++)
+//        m_hb[i + position] = src[offset + i];
+//}
 
 //const uint8_t& ByteBuffer::at(const size_t& position) const {
 //    if (position > _limit)
@@ -158,19 +200,19 @@ void ByteBuffer::modifyMemery(const uint8_t* src, const size_t& offset, const si
 //    return _hb[position];
 //}
 
-ByteBuffer* ByteBuffer::copy(void) const {
-    if (0 >= _capacity || NULL == _hb)
-        return NULL;
-    ByteBuffer* copy = new ByteBuffer(_position, _limit, _capacity, new uint8_t[_capacity]);
-    memcpy(_hb, copy->_hb, _capacity);
-    return copy;
-}
+//ByteBuffer* ByteBuffer::copy(void) const {
+//    if (0 >= _capacity || NULL == _hb)
+//        return NULL;
+//    ByteBuffer* copy = new ByteBuffer(_position, _limit, _capacity, new uint8_t[_capacity]);
+//    memcpy(_hb, copy->_hb, _capacity);
+//    return copy;
+//}
 
 uint8_t* ByteBuffer::array(void) const {
-    if (_hb == NULL)
+    if (m_hb == NULL)
         throw ByteBufferException("ByteBuffer::array(): UnsupportedOperation");
-    return _hb;
-
+    
+    return m_hb;
 }
 
 string ByteBuffer::getString() const {
@@ -178,8 +220,8 @@ string ByteBuffer::getString() const {
 }
 
 string ByteBuffer::getString(const size_t& offset, const size_t& length) const {
-    checkBounds(offset, length, remaining());
-    string str((char*) &_hb[_position + offset], length);
+    checkRemaining(offset, length);
+    string str((char*) &m_hb[m_position + offset], length);
     return str;
 }
 
@@ -188,9 +230,9 @@ void ByteBuffer::outputAsHex(ostream& out) const {
 }
 
 void ByteBuffer::outputAsHex(ostream& out, const size_t& offset, const size_t& length) const {
-    checkBounds(offset, length, remaining());
+    checkRemaining(offset, length);
     for (size_t i = 0; i < length; i++)
-        out << hex << (short) _hb[_position + offset + i] << ' ';
+        out << hex << (short) m_hb[m_position + offset + i] << ' ';
     //        out << hex << setw(3) << setfill(' ') << (short) _hb[_position + offset + i];
 }
 //
@@ -203,8 +245,16 @@ void ByteBuffer::outputAsDec(ostream& out) const {
 }
 
 void ByteBuffer::outputAsDec(ostream& out, const size_t& offset, const size_t& length) const {
-    checkBounds(offset, length, remaining());
+    checkRemaining(offset, length);
     for (size_t i = 0; i < length; i++)
         //        out << (short) _hb[_position + offset + i] << ' ';
-        out << setw(3) << setfill(' ') << (short) _hb[_position + offset + i];
+        out << setw(3) << setfill(' ') << (short) m_hb[m_position + offset + i];
+}
+
+void ByteBuffer::readOnly(const bool& readOnly) {
+    m_readOnly = readOnly;
+}
+
+const bool& ByteBuffer::readOnly() const {
+    return m_readOnly;
 }
--- ByteBuffer/src/ByteBuffer.h
+++ ByteBuffer/src/ByteBuffer.h
@@ -53,15 +53,20 @@ namespace bytebuf {
         }
     };
 
+    /**
+     * 若构造方式为包装已有数组(warp)，则复制构造函数不复制数组元素，只复制数组地址；析构函数不释放数组元素。
+     * 否则即分配空间创建新数组(allocate)，则复制构造函数不创建新数组并复制数组元素；析构函数释放数组元素。
+     */
     class ByteBuffer {
     public:
 
-        //        ByteBuffer(const size_t& size);
-        //        ByteBuffer(byte* src, const size_t& length);
+        ByteBuffer(const size_t& capacity);
+        ByteBuffer(uint8_t* src, const size_t& offset, const size_t& length);
+        ByteBuffer(uint8_t* src, const size_t& length);
+
         ByteBuffer(const ByteBuffer& orig);
         virtual ~ByteBuffer();
 
-        void freeMemery(void);
         void flip(void);
         void clear(void);
         bool hasRemaining(void) const;
@@ -70,30 +75,43 @@ namespace bytebuf {
         void put(const uint32_t& src);
         void put(const char* src);
         void put(const std::string& src);
-        void put(const uint8_t* src, const int& offset, const int& length);
-        const uint8_t& get(const size_t& index) const;
-        const uint8_t& get(void);
-        size_t remaining(void) const;
-        const size_t& position(void) const;
-        size_t position(const size_t& newPosition);
+        void put(const uint8_t* src, const size_t& offset, const size_t& length);
+        void put(const ByteBuffer& src, const size_t& offset, const size_t& length);
+        void put(const ByteBuffer& src);
 
-        void movePosition(const size_t& length, const bool isReverse = false);
-        void modifyMemery(const uint8_t* src, const size_t& offset, const size_t& length, const size_t& position);
+        const uint8_t& get(const size_t& index) const;
 
         /**
-         * If you get a ByteBuffer by this function, 
-         * you need to call ByteBuffer::freeMemery() before delete it.
+         * Reads the byte at this buffer's current position, and then increments the position.
+         *
+         * @return  The byte at the buffer's current position
          */
-        static ByteBuffer* allocate(const size_t& capacity);
-        static ByteBuffer* wrap(uint8_t* array, const size_t& offset, const size_t& length, const size_t& capacity);
-        static ByteBuffer* wrap(uint8_t* array, const size_t& capacity);
+        const uint8_t& get(void);
 
-//        const uint8_t& at(const size_t& position) const;
+        /**
+         *
+         * <p> Reads the next two bytes at this buffer's current position,
+         * composing them into a short value according to the current byte order,
+         * and then increments the position by two.  </p>
+         *
+         * @return  The short value at the buffer's current position
+         */
+        const uint16_t& getShort();
 
-        ByteBuffer* copy(void) const;
+        size_t remaining(void) const;
+        const size_t& position(void) const;
+        void position(const size_t& newPosition);
+
+        void movePosition(const size_t& length, const bool isReverse = false);
+//        void modifyMemery(const uint8_t* src, const size_t& offset, const size_t& length, const size_t& position);
+
+
+        //        const uint8_t& at(const size_t& position) const;
+
+        //        ByteBuffer* copy(void) const;
 
         const size_t& capacity(void) const {
-            return _capacity;
+            return m_capacity;
         }
 
         uint8_t* array(void) const;
@@ -104,18 +122,23 @@ namespace bytebuf {
         void outputAsHex(std::ostream& out, const size_t& offset, const size_t& length) const;
         void outputAsDec(std::ostream& out) const;
         void outputAsDec(std::ostream& out, const size_t& offset, const size_t& length) const;
-//        uint8_t* arrayOfPosition() const;
+        void readOnly(const bool& readOnly);
+        const bool& readOnly() const;
+        //        uint8_t* arrayOfPosition() const;
 
     private:
-        uint8_t* _hb;
-        size_t _position;
-        size_t _limit;
-        size_t _capacity;
+        uint8_t* m_hb;
+        size_t m_position;
+        size_t m_limit;
+        size_t m_capacity;
         size_t _mark;
         uint8_t errorCode;
+        bool m_isWarpped;
+        bool m_readOnly;
 
-        ByteBuffer(const size_t& pos, const size_t& lim, const size_t& cap, uint8_t* hb);
-        static void checkBounds(const int& off, const int& len, const int& size);
+        ByteBuffer(const size_t& pos, const size_t& lim, const size_t& cap, uint8_t* hb, const bool& isWrapped);
+//        static void checkBounds(const int& off, const int& len, const int& cap);
+        void checkRemaining(const size_t& bufferOffset, const size_t& length) const;
 
     };
 }
